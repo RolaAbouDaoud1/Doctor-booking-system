469e06b8026788a7c618b1b1261ad4b4448fe6b5
RolaAbouDaoud <rolaaboudawoud1912@gmail.com>
2025-09-13 11:31:09 +0300

    Login, Signup, Forgot Password added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from "./loginPage";
+import ForgotPassword from './forgotPassword';
+import SignUp from './signUp';
+import LandingPage from "./HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return(
+      <Router>
+         <Routes>
+            <Route path="/" element={<LoginPage/>}/>
+            <Route path="/forgotPassword" element={<ForgotPassword/>} />
+            <Route path="/signUp" element={<SignUp/>} />
+            <Route path="/HomePage" element={<LandingPage />} />
+         </Routes>
+      </Router>
+   );
 }
 
 export default App;
