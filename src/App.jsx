19dcc20e1342beb9f48b1fc1d3addf2fa25997c0
dana-ghanem <danaghanem39@gmail.com>
2025-10-13 17:03:26 +0300

    modified code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import{ BrowserRouter as Router,Routes, Route, BrowserRouter} from "react-router-dom";
+import{ BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import DoctorDashboard from "./pages/DoctorDashboard";
 import PatientDashboard from "./pages/PatientDashboard";
 
 function App(){
   return (
-    <BrowserRouter>
-    <div className="App">
+    <Router>
       <Routes>
         <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
         <Route path="/patient-dashboard" element={<PatientDashboard />} />
       </Routes>
-    </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 export default App;
