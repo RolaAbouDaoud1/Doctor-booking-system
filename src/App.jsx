2040a17e04fe0f16a993f4b487557a3593ac20ca
miryanah <163726449+miryanah@users.noreply.github.com>
2025-10-14 11:24:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DoctorProfile from "./dr/DoctorProfile";
-import PatientProfile from "./patient/PatientProfile";
-=======
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DoctorProfile from "./DoctorProfile";
-import PatientProfile from "./PatientProfile";
->>>>>>> c80ad85e4e471be09c017fc211642a29866c423f
+import DoctorProfile from "./pages/DoctorProfile";
+import PatientProfile from "./pages/PatientProfile";
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-        {/* :doctorId means dynamic parameter */}
+       
         <Route path="/doctor/:doctorId/profile" element={<DoctorProfile />} />
-        
-        {/* :patientId dynamic parameter */}
         <Route path="/patient/:patientId/profile" element={<PatientProfile />} />
-=======
-        <Route path="/doctors/:id" element={<DoctorProfile />} />
-        <Route path="/patients/:id" element={<PatientProfile/>} />
->>>>>>> c80ad85e4e471be09c017fc211642a29866c423f
       </Routes>
     </Router>
   );
